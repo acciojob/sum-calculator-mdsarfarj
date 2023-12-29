3489bf28b60497a3f1caf85a01d211e8428986d4
sakib <sakibm008@gmail.com>
2023-12-29 21:06:22 +0530

    fine

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './../styles/App.css';
 const App = () => {
 
   const [numbers, setNumbers] = useState([]);
-const [sum,setSum]=useState(3);
+const [sum,setSum]=useState(0);
 const handleInputChange = (event) => {
   const value = parseFloat(event.target.value);
   if (!isNaN(value)) {
@@ -18,14 +18,14 @@ const calculateSumAsync = () => {
   setTimeout(() => {
     const calculatedSum = numbers.reduce((acc, num) => acc + num, 0);
     setSum(calculatedSum);
-  }, 300); // Adjust the delay as needed
+  }, 100); // Adjust the delay as needed
 };
   return (
     <div>
         {/* Do not remove the main div */}
         <h1>Sum calculator</h1>
         <input type="number" onChange={handleInputChange}/>
-        <p>Sum: {sum}</p>
+        <p>Sum:{sum}</p>
     </div>
   )
 }
