139e180821164e30af34e8e77f1c5ced5624e7f2
sakib <sakibm008@gmail.com>
2023-12-29 21:12:18 +0530

    jjjjjjjjjjj

--- src/components/App.js
+++ src/components/App.js
@@ -18,14 +18,14 @@ const calculateSumAsync = () => {
   setTimeout(() => {
     const calculatedSum = numbers.reduce((acc, num) => acc + num, 0);
     setSum(calculatedSum);
-  }, 100); // Adjust the delay as needed
+  }, 4000); // Adjust the delay as needed
 };
   return (
     <div>
         {/* Do not remove the main div */}
         <h1>Sum calculator</h1>
         <input type="number" onChange={handleInputChange}/>
-        <p>Sum:{sum}</p>
+        <p>Sum: {sum}</p>
     </div>
   )
 }
