483e982e2b3b8c1bc21477c9cc708e348155b2db
sakib <sakibm008@gmail.com>
2023-12-29 21:27:35 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
-
-import React,{useState,useEffect} from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-
   const [numbers, setNumbers] = useState([]);
-const [sum,setSum]=useState(0);
-const handleInputChange = (event) => {
-  const value = parseFloat(event.target.value);
-  if (!isNaN(value)) {
-    setNumbers((prevNumbers) => [...prevNumbers, value]);
-    calculateSumAsync();
-  }
-};
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    const calculateSumAsync = () => {
+      const calculatedSum = numbers.reduce((acc, num) => acc + num, 0);
+      setSum(calculatedSum);
+    };
+
+    const timerId = setTimeout(() => {
+      calculateSumAsync();
+    }, 300); // Adjust the delay as needed
+
+    return () => clearTimeout(timerId); // Clear the timer on component unmount or re-render
+  }, [numbers]);
+
+  const handleInputChange = (event) => {
+    const value = parseFloat(event.target.value);
+    if (!isNaN(value)) {
+      setNumbers((prevNumbers) => [...prevNumbers, value]);
+    }
+  };
+
 
 const calculateSumAsync = () => {
   setTimeout(() => {
@@ -25,8 +36,7 @@ const calculateSumAsync = () => {
         {/* Do not remove the main div */}
         <h1>Sum calculator</h1>
         <input type="number" onChange={handleInputChange}/>
-        <p>Sum: {sum}</p>
-    </div>
+        <p>{`Sum: ${sum}`}</p>    </div>
   )
 }
 
